7c64c60242d027c23507c5dd35613e7b5799b7ed
zhouyijie9 <zhouyijie99@foxmail.com>
2021-08-02 10:51:11 +0800

    0802

--- ppr.cpp
+++ ppr.cpp
@@ -10,12 +10,12 @@ using namespace std;
 #define alpha 0.15
 //#define threshold 1e-6
 
-struct Node {
-    vector<int> outNodes;
-    double residue = 0;
-    double tmp_residue = 0;
-    double reserve = 0;
-};
+// struct Node {
+//     vector<int> outNodes;
+//     double residue = 0;
+//     double tmp_residue = 0;
+//     double reserve = 0;
+// };
 
 // int main(int argc, char const *argv[]) //会传入两个参数：argv[1]-edgepath和argv[2]-threshold
 int main()
@@ -26,12 +26,13 @@ int main()
     string edge_path = "/home/zyj/zhou/dataset/web-Google1.txt";
     double threshold = 1e-6;
 
-    vector<Node> nodes; //nodes用来存放点向量
+    //vector<Node> nodes; //nodes用来存放点向量
     int source = 0; //我想用文件中的第一个点作为源点，整个实验最后得到的是另外的点关于它的PPR值
-    long long int jisuancishu = 0;
+    // int sid = 0;
+    // long long int jisuancishu = 0;
 
-    double start_read_file = clock();
-    //读文件
+    double start_read_file_time = clock();
+
     ifstream inFile(edge_path);
     if(!inFile)
     {
@@ -41,90 +42,105 @@ int main()
 
     int vertex_num, edge_num;
     inFile >> vertex_num >> edge_num;
-    nodes.resize(vertex_num);
-    
+
     int u, v;
-    while(inFile >> u >> v)
+    int curr_index = -1;
+    int pos = 0;
+
+    vector<int> start_pos;
+    start_pos.resize(vertex_num + 1, 0);
+
+    vector<int> edges;
+
+    while (inFile >> u >> v)
     {
-        nodes[u].outNodes.emplace_back(v);
+        edges.emplace_back(v);
+        if (u != curr_index)
+        {
+            curr_index = u;
+            start_pos[u] = pos;
+        }
+        pos++;
     }
     inFile.close();
-    nodes[0].residue = 1;
+    double read_file_time = (clock() - start_read_file_time) / CLOCKS_PER_SEC; // 输出1：读文件时间
 
-    double read_file_time = (clock() - start_read_file) / CLOCKS_PER_SEC; // 输出1：读文件时间
-    
+    start_pos[vertex_num] = edge_num;
+
+    vector<double> residues;
+    residues.resize(vertex_num + 1, 0);
+
+    vector<double> tmp_residues;
+    tmp_residues.resize(vertex_num + 1, 0);
+
+    vector<double> reserves;
+    reserves.resize(vertex_num + 1, 0);
+
+    residues[source] = 1.0;
+
+
     int cnt = 0; // 统计迭代次数
-    double start_computing = clock();
 
-    // 开始迭代，根据公式，当每个点的【residue/出度<阈值】时停止迭代
+    double teleport_value;
+    int out_degree;
+    int node_below_thr = 0;
+    int pos_0, pos_1;
+
+    double start_computing_time = clock();
+    // 开始迭代，根据公式，当【每个点】的【residue/出度<阈值】时停止迭代
     // 每个点的residue：有alpha部分转换为自己的reserve，有1-alpha部分平均传递给每个邻居
-    while(1)
+    while (1)
     {
-        int nodeBelowThr = 0; // residue/出度 的值低于阈值的点个数
-
-        for(int u = 0; u < vertex_num; ++u)
+        for (int i = 0; i < vertex_num; i++)
         {
-            Node& node = nodes[u]; // 可以加快速度
+            pos_0 = start_pos[i];
+            pos_1 = start_pos[i + 1];
+            out_degree = pos_1 - pos_0;
 
-            int outDegree = node.outNodes.size();
-            double teleportValue = (1 - alpha) * node.residue / outDegree;
-            for(int i = 0; i < outDegree; i++)
+            teleport_value = (1 - alpha) * residues[i] / out_degree;
+            for (int j = 0; j < out_degree; j++)
             {
-                Node &v = nodes[nodes[u].outNodes[i]];
-                v.tmp_residue += teleportValue;
-                jisuancishu++;
+                int curr_node = edges[pos_0 + j];
+                tmp_residues[curr_node] += teleport_value;
             }
-            node.reserve += alpha * node.residue;
-            node.residue = 0;
+            reserves[i] += alpha * residues[i];
+            // residues[i] = 0;
         }
 
-        double curr_teleportValue = 0;
-        for (int u = 0; u < vertex_num; u++)
+        for(int i = 0; i < vertex_num; i++)
         {
-            Node& node = nodes[u];
+            pos_0 = start_pos[i];
+            pos_1 = start_pos[i + 1];
+            out_degree = pos_1 - pos_0;
 
-            node.residue = node.tmp_residue;
-            node.tmp_residue = 0;
+            residues[i] = tmp_residues[i];
+            tmp_residues[i] = 0;
 
-            if (node.residue / node.outNodes.size() < threshold)
-                nodeBelowThr++;
+            if (residues[i] / out_degree < threshold)
+                node_below_thr++;
         }
 
-        if(nodeBelowThr == vertex_num)
+        if(node_below_thr == vertex_num)
             break;
 
         cnt++;
+        node_below_thr = 0;
     }
 
-    double computing_time = (clock() - start_computing) / CLOCKS_PER_SEC; // 输出2：计算时间
-    
+    double computing_time = (clock() - start_computing_time) / CLOCKS_PER_SEC; // 输出2：计算时间
+
     printf("%s%d\n", "step = ", cnt);
     cout << "加载文件时间：" << read_file_time << "\n计算时间：" << computing_time << endl;
-    cout << "计算次数: " << jisuancishu << endl;
-    // 将运行时间写入文件
-    // string resultPath = "./out/result_ppr_origin.txt";
-    // ofstream fout_1(resultPath, ios::app|ios::out);
-    // fout_1 << "normal_graph_time:" << (clock()-start) / CLOCKS_PER_SEC << endl;
-    // fout_1 << "normal_graph_step:" << cnt << endl;
-    // fout_1.close();
 
-    string outPath = "./out/ppr.txt";
+    string outPath = "./out/ppr2.txt";
     cout << "out path: " << outPath << endl;
     ofstream fout(outPath);
     for(int i = 0; i < vertex_num; i++)
     {
-        fout << i << " residue: " << nodes[i].residue << " reserve: " << nodes[i].reserve << endl;
+        fout << i << " residue: " << residues[i] << " reserve: " << reserves[i] << endl;
     }
 
     fout.close();
-    // cout << "正在将每个点的结果写入文件......\n";
-    // string path1 = "./out/r1.txt";
-    // ofstream fout_1(path1, ios::app|ios::out);
-    // for(int i = 0; i < vertex_num; i++)  
-    // {
-    //     fout_1 << i << ", residue: " << nodes[i].residue << ", reserve: " << nodes[i].reserve << endl;
-    // }
-    // fout_1.close();
     
     return 0;
 }
