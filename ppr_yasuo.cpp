ea5949eb615b0be4f2cba78390401c3b1122c5f9
zhouyijie9 <zhouyijie99@foxmail.com>
2021-08-12 13:00:23 +0800

    0912

--- ppr_yasuo.cpp
+++ ppr_yasuo.cpp
@@ -12,8 +12,9 @@
 #include <stdlib.h>
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main()
 {
+    double r_sum = 1.0;
     double alpha = 0.15;
     double l1_error = 1e-6;
     string v_path = "/home/zyj/zhou/PPR-proj/out/google_yasuo.v"; // 压缩图点文件
@@ -22,11 +23,14 @@ int main(int argc, char const *argv[])
     int real_nodes_num = 0; // 真实点的个数
     int edge_num = 0; // 所有边的条数
     vector<double> residues;
+    vector<double> tmp_residues;
     vector<double> reserves;
     vector<int> weights;
     vector<int> out_adj_nums;
     vector<int> start_pos;
     vector<int> edges;
+    // cout << "顶点个数：" << all_nodes_num << "，真实点个数：" << all_nodes_num << "，边条数：" << edge_num << endl;
+    // printf("顶点个数：%d，真实点个数：%d，边条数：%d", all_nodes_num, real_nodes_num, edge_num);
 
     // 读.v文件
     ifstream inFile_v(v_path);
@@ -73,91 +77,95 @@ int main(int argc, char const *argv[])
     }
     inFile.close();
     //----------------------------------.e文件读完
-    //想获得
 
-    residues.resize(all_nodes_num);
+    // for (int i = 0; i < 20; i++) {
+    //     cout << start_pos[i] << ' ';
+    // }
+
+    cout << "\n顶点个数：" << all_nodes_num << "，真实点个数：" << real_nodes_num << "，边条数：" << edge_num << endl;
+
+    residues.resize(real_nodes_num);
     reserves.resize(real_nodes_num);
-    int source = 0; //我想用文件中的第一个点作为源点，整个实验最后得到的是另外的点关于它的PPR值
+    tmp_residues.resize(all_nodes_num);
+    double threshold_to_reject = l1_error / edge_num; // 这个edge_num应该是原图上边的数量，不是压缩图边的数量。先不改了
+    int source = 0;
     residues[source] = 1.0;
-    printf("edge_num=%d\n", edge_num);
-    double threshold_to_reject = l1_error / 5241298;
-    vector<bool> is_active;
-    is_active.resize(all_nodes_num);
-    double r_sum = 1.0;
-    queue<int> active_nodes_queue;
-    active_nodes_queue.push(source);
-    long long unsigned int number_of_pushes = 0, previous_number_of_pushes = 0;
+
+    int iter_num = 0;
+    long long int push_num = 0, prev_push_num = 0;
     double start_computing_time = clock();
-    long long int while_step = 0;
-    while (!active_nodes_queue.empty() && r_sum >= l1_error) {
-        while_step++;
-        if (number_of_pushes - previous_number_of_pushes >= edge_num) {
-            previous_number_of_pushes = number_of_pushes;
-            size_t num_iter = number_of_pushes / edge_num;
-            printf("#Iter:%s%lu\tr_sum:%.12f\tTime Used:%.4f\t#Pushes:%llu\n",
-                   (num_iter < 10 ? "0" : ""), num_iter, r_sum, (clock() - start_computing_time) / CLOCKS_PER_SEC, number_of_pushes);
-        }
-        int curr_node = active_nodes_queue.front();
-        active_nodes_queue.pop();
-        is_active[curr_node] = false;
-        int pos_0 = start_pos[curr_node];
-        int pos_1 = start_pos[curr_node + 1];
-        int out_degree = pos_1 - pos_0;
-        number_of_pushes += out_degree;
-        double curr_residue = residues[curr_node];
-        residues[curr_node] = 0;
-        double increment = 0;
-        // 以下对真实点和虚拟点分开讨论
-        if (curr_node < real_nodes_num) { // 真实点
-            increment = (1 - alpha) * curr_residue / out_adj_nums[curr_node]; // 比在原图上多了访问out_adj_nums数组的时间
-            reserves[curr_node] += alpha * curr_residue;
-            r_sum -= alpha * curr_residue;
-            for (int j = pos_0; j < pos_1; j++) {
-                int curr_neighbour = edges[j];
-                // 对于邻居也要分真实点和虚拟点进行讨论
-                if (curr_neighbour < real_nodes_num) {
-                    residues[curr_neighbour] += increment;
-                    if (residues[curr_neighbour] >= out_degree * threshold_to_reject && !is_active[curr_neighbour]) {
-                        is_active[curr_neighbour] = true;
-                        active_nodes_queue.push(curr_neighbour);
-                    }
+    while(r_sum >= l1_error)
+    {
+        // if(push_num - prev_push_num >= edge_num) {
+        //     prev_push_num = push_num;
+        //     cout << "iter " << push_num / edge_num << "---------\n";
+        // }
+        // cout << "push_num=" << push_num++ << endl;
+
+        for (int i = 0; i < real_nodes_num; i++) // 将真实点的residue进行push，先存在所有点的tmp_residue里面
+        {
+            // double &curr_residue = residues[i];
+            if(residues[i] < threshold_to_reject)
+                continue;
+            else
+            {
+                int pos_0 = start_pos[i];
+                int pos_1 = start_pos[i + 1];
+                int out_degree = pos_1 - pos_0;
+                push_num += out_degree;
+                double increment = residues[i] * (1 - alpha) / out_adj_nums[i];
+                // cout << residues[i] << "---" << 1 - alpha << "---" << out_adj_nums[i] << endl;
+                // cout << "increment=" << increment << endl;
+                for (int j = pos_0; j < pos_1; j++)
+                {
+                    int curr_neighbour = edges[j];
+                    tmp_residues[curr_neighbour] += increment * weights[curr_neighbour];
                 }
-                else {
-                    residues[curr_neighbour] += increment * weights[curr_neighbour]; // 比在原图上多了访问weights数组的时间
-                    if (!is_active[curr_neighbour]) {
-                        is_active[curr_neighbour] = true;
-                        active_nodes_queue.push(curr_neighbour);
-                    }
-                }
+                reserves[i] += alpha * residues[i];
+                r_sum -= alpha * residues[i];
+                residues[i] = 0;
             }
         }
-        else { // 虚拟点，邻居全是真实点
-            increment = curr_residue / out_degree;
-            for (int j = pos_0; j < pos_1; j++) {
+        // cout << tmp_residues[1] << endl;
+        // cout << tmp_residues[2] << endl;
+        // cout << tmp_residues[928349] << endl;
+        // return 0;
+        for (int i = 0; i < real_nodes_num; i++) // 把真实点的tmp_residue加到它们自身的residue上
+        {
+            if(tmp_residues[i] == 0)
+                continue;
+            // double &tmp_residue_ = tmp_residues[i];
+            residues[i] += tmp_residues[i];
+            tmp_residues[i] = 0;
+        }
+        
+        for(int i = real_nodes_num; i < all_nodes_num; i++) // 把虚拟点的tmp_residue进行push，直接加到真实点的residue上
+        {
+            if(tmp_residues[i] == 0)
+                continue;
+            int pos_0 = start_pos[i];
+            int pos_1 = start_pos[i + 1];
+            int out_degree = pos_1 - pos_0;
+            push_num += out_degree;
+            // double &tmp_residue_ = tmp_residues[i];
+            double increment = tmp_residues[i] / out_degree;
+            for (int j = pos_0; j < pos_1; j++)
+            {
                 int curr_neighbour = edges[j];
                 residues[curr_neighbour] += increment;
-                if (residues[curr_neighbour] >= out_degree * threshold_to_reject && !is_active[curr_neighbour]) {
-                    is_active[curr_neighbour] = true;
-                    active_nodes_queue.push(curr_neighbour);
-                }
             }
+            tmp_residues[i] = 0;
         }
     }
-    cout << "while_step="<<while_step << endl;
-    const size_t num_iter = number_of_pushes / edge_num;
-    printf("#Iter:%s%lu\tr_sum:%.12f\tTime Used:%.4f\t#Pushes:%llu\n",
-           (num_iter < 10 ? "0" : ""), num_iter, r_sum, (clock() - start_computing_time) / CLOCKS_PER_SEC, number_of_pushes);
-    double computing_time = (clock() - start_computing_time) / CLOCKS_PER_SEC; // 输出2：计算时间
-    printf("r_sum:%.12f\tTime Used:%.4f\n", r_sum, computing_time);
-    // string outPath = "./out/ppr_yasuo3.txt";
-    // cout << "out path: " << outPath << endl;
-    // ofstream fout(outPath);
-    // for(int i = 0; i < real_nodes_num; i++)
-    // {
-    //     fout << i << " residue: " << residues[i] << " reserve: " << reserves[i] << endl;
-    // }
-    // fout.close();
+    cout << "computing time: " << (clock() - start_computing_time) / CLOCKS_PER_SEC << endl;
+    // cout << "r_sum: " << r_sum << endl;
+    string outPath = "./out/ppr_yasuo4.txt";
+    cout << "out path: " << outPath << endl;
+    ofstream fout(outPath);
+    for (int i = 0; i < real_nodes_num; i++) {
+        fout << i << " residue: " << residues[i] << " reserve: " << reserves[i] << endl;
+    }
+    fout.close();
 
-    
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -4,21 +4,10 @@
 using namespace std;
 int main()
 {
-    double start1 = clock();
-    for (int i = 0; i < 1303995284; i++)
-    {
-        double m = 1238.9;
-        m = m * i;
-    }
-    cout << "t1 = " << (clock() - start1) / CLOCKS_PER_SEC << endl;
-
-    double start2 = clock();
-    for (int i = 0; i < 1303995284; i++)
-    {
-        double m = 1238.9;
-        m = m + i;
-    }
-    cout << "t2 = " << (clock() - start2) / CLOCKS_PER_SEC << endl;
+    int b = 10;
+    int &a = b;
+    a = 20;
+    cout << "a=" << a << ",b=" << b << endl;
 
     return 0;
 }
